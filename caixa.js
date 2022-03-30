768c35170dd386cc1455a8a8f079a932db63fe07
John Rodrigues <johnlcrodrigues@gmail.com>
2022-03-30 20:03:55 -0300

    subindo arquivos

---
+++ caixa.js
@@ -0,0 +1,19 @@
+const arrayCedula = []
+
+function efetuar(){
+    let elementoEfetuar = document.getElementById("saqueValor");
+    let resto = elementoEfetuar % 5;
+    console.log(resto)
+    /*if (resto == 0 ){
+        console.log("teste passed")
+        
+    } else{
+
+        window.alert('Só são possiveis saques  de valores multiplos de 5 reais');
+*/
+
+    }
+
+}
+
+
