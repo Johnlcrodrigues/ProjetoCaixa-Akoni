36325ffddad36c99330207c22fe9721b12275a0f
John Rodrigues <johnlcrodrigues@gmail.com>
2022-03-30 21:00:30 -0300

    subindo arquivos 2

--- caixa.js
+++ caixa.js
@@ -1,10 +1,14 @@
 const arrayCedula = []
 
-function efetuar(){
+function efetuar() {
     let elementoEfetuar = document.getElementById("saqueValor");
-    let resto = elementoEfetuar % 5;
-    console.log(resto)
-    /*if (resto == 0 ){
+   
+   
+   
+     let resto = elementoEfetuar.value % 5;
+    /*
+    console.log(resto);
+    if (resto == 0 ){
         console.log("teste passed")
         
     } else{
@@ -12,8 +16,44 @@ function efetuar(){
         window.alert('Só são possiveis saques  de valores multiplos de 5 reais');
 */
 
-    }
+while (saldo > 0) {
+
+switch (saldo) {
+    case (saldo >100):
+        qtdeCedulas = int(saldo/100);
+        saldo = saldo % 100
+        arrayCedula.push[qtdeCedulas,100]
+        
+    case (saldo >50):
+        qtdeCedulas = int(saldo/50);
+        saldo = saldo % 50;
+        arrayCedula.push[qtdeCedulas,50];
+            
+    case (saldo >20):
+        qtdeCedulas = int(saldo/20);
+        saldo = saldo % 20;
+        arrayCedula.push[qtdeCedulas,20];
+            
+    case (saldo >10):
+        qtdeCedulas = int(saldo/10);
+        saldo = saldo % 10
+        arrayCedula.push[qtdeCedulas,10]
+
+    case (saldo >5):
+        qtdeCedulas = int(saldo/5);
+        saldo = saldo % 5;
+        arrayCedula.push[qtdeCedulas,5];
+                
+            
+    default:
+
+
+
+
 
 }
 
+console.log(arrayCedula[1.1]);
+   
+};
 
